9a7da7f457d84f5e111e9bf5e8c11837033b1640
luchinhsun <lu.chinhsun@gmail.com>
2017-10-23 00:23:39 +0800

    102317
    heat equation exercise

---
+++ heat.py
@@ -0,0 +1,12 @@
+''' Heat equation exercise
+creat by chinhsun,LU '''
+
+import numpy as np
+nx = 50;
+ny = 50;
+V = np.zeros((nx,ny))
+Vnew = np.zeros((nx,ny))
+'''for i in range(nx):
+    for j in range(ny):
+        a[i,j] = i*nx+j
+print(range(nx))'''
